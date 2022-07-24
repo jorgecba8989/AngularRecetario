e0f3363635f61b394cc745df7255733efeaa9f93
jorgecba8989 <georgecba89@gmail.com>
2022-07-24 15:11:19 -0300

    eliminacion de codigo

--- src/app/services/api-marvel.service.ts
+++ src/app/services/api-marvel.service.ts
@@ -28,9 +28,4 @@ export class ApiMarvelService {
     return this.http.get(comicUrl);
   }
 
-  getCharacterByName(characterName:string):Observable<any>
-  {
-    const characterBYNameUrl = `https://gateway.marvel.com:443/v1/public/characters?name=${characterName}&ts=1&apikey=0e3f9405763aaa9bb2211f7887df40e8&hash=dfe0d84751cdf512c2ea6349a8b4b664`;
-    return this.http.get(characterBYNameUrl);
-  }
 }
