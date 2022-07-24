89d9d8a362b06632abaf4458a0dbb11f20c8b298
jorgecba8989 <georgecba89@gmail.com>
2022-07-24 15:11:07 -0300

    eliminacion de codigo

--- src/app/mis-favoritos/mis-favoritos.component.ts
+++ src/app/mis-favoritos/mis-favoritos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { ApiMarvelService } from '../services/api-marvel.service';
 
 @Component({
@@ -14,39 +15,49 @@ export class MisFavoritosComponent implements OnInit {
   showSearchResult:boolean= false;
   searchedCharacter:any=[];
   formularioBusqueda!: FormGroup;
+  favoritos: any[] = new Array<any>();
+
 
   constructor(private service:ApiMarvelService,
+    private db: AngularFirestore
               // public fb:FormBuilder,
               ) { }
 
   ngOnInit(): void {
-    // this.formularioBusqueda = new FormGroup({
-    //   buscador:new FormControl (""),
-     
-    // })
+    this.favoritos.length = 0;
+    this.db.collection('comics').get().subscribe((resultado)=>{
+
+      for(let item of resultado.docs){
+          console.log(item.id) 
+          
+          let favoritoBD: any  = item.data() ;
+          favoritoBD.id = item.id;
+          this.favoritos.push(favoritoBD)
+      }
+    })
   }
 
-  // buscar(nombreBuscar : number){
-  //   console.log(nombreBuscar)
-  //   this.service.comicSeleccionado(nombreBuscar).subscribe((result)=>{
-  //     this.comicBuscado = result.data.results;
-  //     console.log(this.comicBuscado)
-  //   })
-  // }
+
 
   buscar(event : any){
-    this.characterName = event.target.value;
-    console.log(this.characterName);
-    // let _valor = this.formularioBusqueda.get("buscador")?.value
-    this.service.getCharacterByName(this.characterName).subscribe((result)=>{
-      console.log(result);
-      if(result.data.count>0)
+    this.favoritos.forEach((f)=>{
+      if(f.titulo.toLowerCase().includes(event.toLowerCase()))
       {
-        this.showSearchResult = true;
-        this.searchedCharacter = result.data.results;
-      }
-     
+        this.characterName = f.titulo;
+        let obtenerIdComic = f.idComic
+        this.service.comicSeleccionado(obtenerIdComic).subscribe((result)=>{
+          if(result.data.count>0)
+          {
+            this.showSearchResult = true;
+            this.searchedCharacter = result.data.results;
+          }
+        })
+        }
+      // else{
+       
+      // }
     })
+
   }
 
 }
